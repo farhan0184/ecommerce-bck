52511df30eb07a05b3fa930200080368c90c2ef4
farhan0184 <shahariar0184@gmail.com>
2024-12-20 03:58:21 +0600

    complete

--- dist/routes/api/user.routers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_validators_1 = require("../../validators/user.validators");
-const auth_controlers_1 = require("../../controllers/auth.controlers");
-const userRoutes = (0, express_1.Router)();
-userRoutes.post("/register", user_validators_1.validateUserRegister, auth_controlers_1.createUser);
-exports.default = userRoutes;
-//# sourceMappingURL=user.routers.js.map
